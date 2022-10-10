57a000465ca6a750e5013768a1522dfa83359827
SeungyoupBaek <bsy8220@naver.com>
2022-10-10 17:08:31 +0900

    feat(util): Modify validationUtils
    Add validation names function to ValidationUtils
    - split names and check name length between 0 and 5
    - add names validation function test
    
    Add IllegalArgumentException control
    - throw exception if not meet the requrementsˆ

--- src/main/java/racingcar/util/ValidationUtils.java
+++ src/main/java/racingcar/util/ValidationUtils.java
@@ -1,16 +1,29 @@
 package racingcar.util;
 
 public class ValidationUtils {
-    public static final int MIN_no = 0;
+    public static final int MIN_NO = 0;
     public static final int MAX_NO = 9;
     public static final int MIN_NAME_LENGTH = 0;
     public static final int MAX_NAME_LENGTH = 5;
 
-    public static boolean validNo(int no) {
-        return no >= MIN_no && no <= MAX_NO;
+    public static boolean validNo(int no) throws IllegalArgumentException {
+        if (no < MIN_NO || no > MAX_NO) {
+            throw new IllegalArgumentException();
+        }
+        return true;
     }
 
     public static boolean validName(String name) {
         return name.length() > MIN_NAME_LENGTH && name.length() <= MAX_NAME_LENGTH;
+    }
+
+    public static boolean validNames(String racingCarNamesInput) {
+        String[] names = racingCarNamesInput.split(",");
+        for (String name : names) {
+            if (name.length() > 5 || name.length() < 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
     }
 }
--- src/test/java/racingcar/util/ValidationUtilsTest.java
+++ src/test/java/racingcar/util/ValidationUtilsTest.java
@@ -1,6 +1,8 @@
 package racingcar.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +14,8 @@ public class ValidationUtilsTest {
     void 입력값_0부터_9사이_검증() {
         assertThat(ValidationUtils.validNo(9)).isTrue();
         assertThat(ValidationUtils.validNo(0)).isTrue();
-        assertThat(ValidationUtils.validNo(-1)).isFalse();
-        assertThat(ValidationUtils.validNo(10)).isFalse();
+        assertThatIllegalArgumentException().isThrownBy(() -> ValidationUtils.validNo(-1));
+        assertThatIllegalArgumentException().isThrownBy(() -> ValidationUtils.validNo(10));
     }
 
     @Test
@@ -23,5 +25,13 @@ public class ValidationUtilsTest {
         assertThat(ValidationUtils.validName("abcde")).isTrue();
         assertThat(ValidationUtils.validName("abcdef")).isFalse();
         assertThat(ValidationUtils.validName("")).isFalse();
+    }
+
+    @Test
+    @DisplayName("입력된_이름_각각의_길이가_5자이하_검증")
+    void 입력된_이름_각각의_길이가_5자이하_검증() {
+        assertThat(ValidationUtils.validNames("pobi,neo,ruke")).isTrue();
+        assertThatIllegalArgumentException().isThrownBy(() -> ValidationUtils.validNames("pabian,neo,ruke"));
+        assertThatIllegalArgumentException().isThrownBy(() -> ValidationUtils.validNames(""));
     }
 }
